32790433d309014adb1781b4d622932510950b6a
Ishan Khare <ishankhare07@gmail.com>
2020-11-29 00:11:17 +0530

    define crd type and spec
    Signed-off-by: Ishan Khare <ishankhare07@gmail.com>

--- api/v1alpha1/cron_types.go
+++ api/v1alpha1/cron_types.go
@@ -7,22 +7,41 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
+const (
+	PhasePending    = "PENDING"
+	PhaseRunning    = "RUNNING"
+	PhaseRegistered = "REGISTERED"
+	PhaseDone       = "DONE"
+)
+
+type SecretSpec struct {
+	Name      string `json:"name,omitempty"`
+	MountAs   string `json:"mount_as,omitempty"`   // volume or env
+	MountPath string `json:"mount_path,omitempty"` // only considered in case of mount as volume
+}
+
 // CronSpec defines the desired state of Cron
 type CronSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of Cron. Edit Cron_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// crontab syntax based schedule
+	Schedule string     `json:"schedule,omitempty"`
+	Secret   SecretSpec `json:"secret,omitempty"`
 }
 
 // CronStatus defines the observed state of Cron
 type CronStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	Phase        string `json:"phase,omitempty"`
+	LastExecuted string `json:"last_executed,omitempty"`
+	IsActive     bool   `json:"is_active,omitempty"`
 }
 
 // +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
 
 // Cron is the Schema for the crons API
 type Cron struct {
